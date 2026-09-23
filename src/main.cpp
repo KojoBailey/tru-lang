909c8c83b849e5a29d712428a631619c9ee8701a
Kojo Bailey <kojomolojo@gmail.com>
2026-09-23 03:45:17 +0100

    Test AST by printing example

--- src/main.cpp
+++ src/main.cpp
@@ -93,6 +93,7 @@ struct Import {
 };
 
 class Component {
+public:
 	std::Variant<
 		MemberDeclaration,
 		Import
@@ -106,8 +107,55 @@ public:
 
 auto main() -> std::CInt
 {
-	auto expr = Expression{Identifier{"foobar"}};
-	std::println("{}", std::get<Identifier>(expr.node).name);
+	FunctionCall helloWorld = {
+		.callee = Identifier{"printLine"},
+		.args = {{Identifier{"0"}, Expression{StringLiteral{"Hello, world!"}}}},
+	};
+
+	ExpressionSequence mainFunctionBody;
+	mainFunctionBody.expressions.emplace_back(Expression{helloWorld});
+
+	Expression mainFunctionBodyExpr = Expression{mainFunctionBody};
+	MemberDeclaration mainFunction = MemberDeclaration{
+		.isLocal = false,
+		.name = Identifier{"run"},
+		.type = nullptr,
+		.defaultValue = &mainFunctionBodyExpr,
+	};
+	Interface program;
+	program.components.emplace_back(Component{mainFunction});
+
+	std::String printBuffer = "== PROGRAM START ==\n\n";
+	if (std::holds_alternative<MemberDeclaration>(program.components[0].component)) {
+		auto memberDeclaration = std::get<MemberDeclaration>(program.components[0].component);
+		if (memberDeclaration.isLocal) {
+			printBuffer += "local ";
+		}
+		printBuffer += memberDeclaration.name.name + " ";
+		printBuffer += "# ...";
+		if (memberDeclaration.defaultValue.has_value() and *memberDeclaration.defaultValue != nullptr) {
+			printBuffer += " = ";
+			if (std::holds_alternative<ExpressionSequence>((**memberDeclaration.defaultValue).node)) {
+				printBuffer += "() -> ():\n";
+				auto expressionSequence = std::get<ExpressionSequence>((**memberDeclaration.defaultValue).node);
+				for (auto& expression : expressionSequence.expressions) {
+					printBuffer += "\t";
+					if (std::holds_alternative<FunctionCall>(expression.node)) {
+						auto functionCall = std::get<FunctionCall>(expression.node);
+						printBuffer += functionCall.callee.name + "; ";
+						auto& firstArg = functionCall.args.at(Identifier{"0"});
+						if (std::holds_alternative<StringLiteral>(firstArg.node)) {
+							auto stringLiteral = std::get<StringLiteral>(firstArg.node);
+							printBuffer += "\"" + stringLiteral.contents + "\"";
+						}
+					}
+				}
+			}
+		}
+		printBuffer += "\n";
+	}
+	printBuffer += "\n== PROGRAM END ==\n";
+	std::print("{}", printBuffer);
 
     return 0;
 }
